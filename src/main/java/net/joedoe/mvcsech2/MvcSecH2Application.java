aba41cb73905981784c71107b030695e14b35401
j-o-e-d-o-e <d_joe@gmx.net>
2018-05-23 16:59:22 +0200

    Initial commit

---
+++ src/main/java/net/joedoe/mvcsech2/MvcSecH2Application.java
@@ -0,0 +1,12 @@
+package net.joedoe.mvcsech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcSecH2Application.class, args);
+    }
+}
