ab1d7ec53581b3b392c872e65692f96e88f5feac
j-o-e-d-o-e <d_joe@gmx.net>
2019-08-30 15:35:35 +0200

    Remote db config

---
+++ src/main/resources/bootstrap.sql
@@ -0,0 +1,17 @@
+insert into product (name, description, icon, color, price) values
+('Phone', 'Mobile device', 'mobile-alt', '#FF6766', 495.99),
+('Laptop', 'For work and leisure', 'laptop', '#97CE68', 1499.95),
+('Headphones', 'Quiet comfort', 'headphones', '#4BA6E0', 249.95),
+('Concert', '2x concert tickets', 'music', '#ffb366', 79.90),
+('Bicycle', 'Two wheels, one saddle', 'bicycle', 'green', 889.95),
+('Cinema', '2x movie tickets', 'film', 'dimgray', 20.00),
+('Gamepad', 'For fun', 'gamepad', '#FF6766', 19.95);
+
+insert into user(first_name, last_name, password, username) values
+('Mary', 'Jane', 'mary', 'jane'),
+('Joe', 'Doe', 'joe', 'doe');
+
+insert into address(street, zip_code, city) values
+('Bourbon St 3', '12345', 'Seville'),
+('Evergreen Terrace 667', '13813', 'Metropolis');
+
